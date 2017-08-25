ad45491e44fc77feb937f5489d0aefe0815aeba3
deepdhungel <31308573+deepdhungel@users.noreply.github.com>
2017-08-25 12:28:02 +0200

    Add files via upload

---
+++ pi_chart_analysis_of_Gender.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt 
+import pandas as pd 
+
+df = pd.read_csv('C:\Users\OnoTation\Desktop\AllCsvs\cocktail_ids_age_gender.csv') 
+df.values
+
+genders = df.Gender.unique()
+
+counts = df.groupby(['Gender','Age']).count()
+
+totals = counts.sum(level = 0) 
+print totals
+
+temp2 = df.groupby('Gender').Cocktail_ID.count()
+ax = temp2.plot(kind='pie',stacked = False, colormap = 'Paired',figsize=(6, 6),autopct='%.2f',fontsize=10)
+ax.legend(loc=3, labels=None)
+plt.title('Distribution Of Cocktails By Gender',fontsize=40)
+plt.ylabel('')
+
+
+plt.show()
+
